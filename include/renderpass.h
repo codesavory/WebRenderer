a2e68be4ed27abeb31363dc9da53977657f54213
Suriya Dakshina Murthy <suriya_daxna@Suriyas-Mac-mini.local>
2024-12-08 10:16:06 -0800

    renderpass: added implementation

--- include/renderpass.h
+++ include/renderpass.h
@@ -12,10 +12,32 @@ class RenderPass {
 public:
     RenderPass();
     ~RenderPass();
-
+    
+    /**
+     * Begin the render pass
+     *
+     * The specified drawing state (e.g. depth tests, culling mode, blending mode) are automatically set up at this
+     * point. Later changes between \ref begin() and \ref end() are possible but cause additional OpenGL/GLES/Metal API
+     * calls.
+     */
     void begin();
+    
+    // Finish the render pass
     void end();
+    
+    // Set the pixel offset and size of the viewport region
+    void set_viewport(const int2 &offset, const int2 &size);
 
 protected:
-
+    bool m_clear;
+    float4 m_clear_color;
+    int2 m_viewport_offset;
+    int2 m_viewport_size;
+    int2 m_framebuffer_size;
+    bool m_active;
+    
+    // FUTURE: depth buffer, culling, blendering and scissor test
+    
+    // OpenGL vars
+    int4 m_viewport_backup;
 };
--- src/renderpass.cpp
+++ src/renderpass.cpp
@@ -1,7 +1,45 @@
 #include "hello_imgui/hello_imgui_include_opengl.h" // cross-platform way to include OpenGL headers
-//#include "opengl_check.h"
 #include "renderpass.h"
 
 #include <fmt/core.h>
 #include <stdexcept>
 
+RenderPass::RenderPass() {
+    m_viewport_size = m_framebuffer_size = int2{0, 0};
+}
+
+RenderPass::~RenderPass() {
+    
+}
+
+void RenderPass::begin() {
+    m_active = true;
+    
+    // Save the current viewport
+    CHK(glGetIntegerv(GL_VIEWPORT, &m_viewport_backup[0]));
+    
+    // Set the viewport for rendering
+    set_viewport(m_viewport_offset, m_viewport_size);
+    
+    // clear the color buffer
+    CHK(glClear(m_clear_color.x, m_clear_color.y, m_clear_color.z, m_clear_color.w));
+    CHK(glClear(GL_COLOR_BUFFER_BIT));
+    
+}
+
+void RenderPass::end() {
+    // Restore the original viewport
+    CHK(glViewport(m_viewport_backup[0], m_viewport_backup[1], m_viewport_backup[2], m_viewport_backup[3]));
+    
+    m_active = false;
+}
+
+void RenderPass::set_viewport(const int2 &offset, const int2 &size) {
+    m_viewport_offset = offset;
+    m_viewport_size = size;
+    
+    if(m_active) {
+        int ypos = m_framebuffer_size.y - m_viewport_size.y - m_viewport_offset.y;
+        CHK(glViewport(m_viewport_offset.x, ypos, m_viewport_size.x, m_viewport_size.y));
+    }
+}
